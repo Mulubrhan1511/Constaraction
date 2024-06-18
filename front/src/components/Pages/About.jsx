c44ddf77f36ab54d1a90d25e6df1a4cfe29e5b85
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2024-06-18 13:51:40 +0300

    edit the hero section

--- front/src/components/Pages/About.jsx
+++ front/src/components/Pages/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Hero2 from '../Hero/Hero2'
-import Team from '../Team.jsx/Team'
+import mission from "../../assets/About/misiiion.png"
+import vision from "../../assets/About/vision.png"
 
 const About = () => {
   return (
@@ -9,72 +9,18 @@ const About = () => {
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative text-center">
         <h1
             class="max-w-2xl mx-auto text-center font-manrope font-bold text-4xl  text-gray-900 mb-5 md:text-5xl md:leading-normal">
-            About <span class="text-blue-400">us </span>
+            About <span class="text-blue-400">Darna capital </span>
         </h1>
-        <p class="max-w-5xl mx-auto text-center text-base lg:text-2xl font-normal leading-7 text-gray-500 mb-9 ">DARNA Construction
-        DARNA Construction and Business PLC was established at 2024 GC at Mekelle city, Tigray regional state, Ethiopia. 
-        It is a private limited company that involves general construction works like building commercial and residential 
-        construction, road and dam construction, site work and finishing works. DARNA Construction and Business plc will 
-        targets in the near future fully involve in Real Estate development. DARNA Construction and Business plc believe 
-        the most company which makes a difference in construction and real estate development bulk construction, best quality
-         construction and safety construction for the clients. DARNA will use modern technology, very qualified professionals and
-          satisfied clients in East Africa.
-          <br/>
-          <br/>
-
-          In addition DARNA involve in business like import construction materials and industries raw materials as well as it
-           will supply construction materials in local market integrated with local manufacturers.
-           <br/>
-           <br/>
-           DARNA construction and business plc use 500,000 birr for startup finance. This finance is very small but it expected
-            the founders experience and connection as well as it involve very capable engineers in the company.
-DARNA construction and business plc will focus urbanization of the country and its neighbor country is low. In Ethiopia urbanization
- is around 23% as censes of 2010 E.C. So from the rural area people migrated highly. Not only migration but also in the urban area the
- demand of housing and infrastructure is big. The modern construction in urban area is not well. DARNA construction and business 
- plc need its own contribution in Ethiopia and neighboring country which is East Africa. 
-        <h1
-         class="max-w-2xl mx-auto text-center font-manrope  text-4xl  text-gray-900 mb-5 md:text-5xl md:leading-normal">
-            Background
-        </h1>
-
- DARNA Construction and Business plc will focus on resident, commercial and infrastructure. DARNA Construction and 
- Business plc is a construction and business organization established to construct building, sidewalk, road, highways
-  and real estate development. 
-DARNA Construction and Business plc will handle architectural design, building actual structure and doing contract administration. 
-And DARNA Construction and Business plc have a team of experts includes architects, engineers, project managers and officers who work 
-together.
-<br/>
-<br/>
-
-<h1
-         class="max-w-2xl mx-auto text-center font-manrope  text-2xl  text-gray-900 mb-5 md:text-3xl md:leading-normal">
-            ORGANIZATION STRUCTURE OF DARNACONSTRUCTION AND BUSINESS PLC IS THE FOLLOWING:-
-
-        </h1>
-        The founders of DARNA construction and business PLC are two Ethiopian. The general manager is Mrs. 
-        Almaz Abreha. She has accounting BA Degree background. She has good 
-        experience in trading by her own way. The other founder and D/Manager
-         is Mr. Nigus Teklay his background was BA Degree in foreign 
-         languages and literature and MA degree in urban management. He has
-          good experience at communication and project management. He worked 
-          around 10 years in government offices as project manager and bureau
-           head as well as he has  good experience at import, export and 
-           electro mechanical. He worked in two private companies as general
-            manager. So the founders have good experience in construction,
-             logistics, import, export and communication. DARNA will have a
-              chance to support by good experienced young founders. 
-              The founders are around 5 year in their own business and
-               accessing business.
-
-
-</p>
+        <p class="max-w-sm mx-auto text-center text-base font-normal leading-7 text-gray-500 mb-9">DARNA specializes in a 
+            wide range of construction projects, including residential, commercial, and industrial developments. The company is known for its commitment to 
+            quality, safety, and innovation, consistently delivering projects that exceed client expectations.</p>
     </div>
 </section>
 <section class="py-14 lg:py-24 relative">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative ">
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-9">
             <div class="img-box">
-                <img src="../../assets/About/vision.png" alt="About Us tailwind page"
+                <img src={vision} alt="About Us tailwind page"
                     class="max-lg:mx-auto" />
             </div>
             <div class="lg:pl-[100px] flex items-center">
@@ -82,8 +28,11 @@ together.
                     <h2
                         class="font-manrope font-bold text-4xl lg:text-5xl text-black mb-9 max-lg:text-center relative">
                         Vision</h2>
-                    <p class="font-normal text-1xl leading- text-gray-500 lg:text-2xl max-lg:text-center max-w-2xl mx-auto">
-                    Our vision is to strive and contract being one of the best construction company and service providers in East Africa
+                    <p class="font-normal text-xl leading-8 text-gray-500 max-lg:text-center max-w-2xl mx-auto">
+                    To be a leader in the construction industry, recognized for:
+Innovation and sustainability: Pioneering new building methods and technologies that minimize environmental impact.
+Exceptional client experiences: Building trust and exceeding expectations through clear communication and collaborative partnerships.
+Positive community impact: Contributing to the development of thriving and sustainable communities.
                     </p>
                 </div>
             </div>
@@ -96,25 +45,26 @@ together.
 
             <div class="lg:pr-24 flex items-center">
                 <div class="data w-full">
-                    {/* <img src="https://collection.cloudinary.com/diulu4bz3/0ee01b927d74cce852c34aa760ff5424" alt="About Us tailwind page"
-                        class="block lg:hidden mb-9 mx-auto" /> */}
-                    <h2 class="font-manrope font-bold text-4xl lg:text-5xl  text-black mb-9 max-lg:text-center">Mission</h2>
-                    <p class="font-normal text-xl leading-8 text-gray-500 lg:text-2xl max-lg:text-center max-w-2xl mx-auto">
-                    DARNA Construction and Business plc aim to be best construction company providing efficien,t affordable,
-                     sustainable, and cost effective services of highest level of quality standard.
+                    <img src={mission} alt="About Us tailwind page"
+                        class="block lg:hidden mb-9 mx-auto" />
+                    <h2 class="font-manrope font-bold text-4xl lg:text-5xl text-black mb-9 max-lg:text-center">Mission</h2>
+                    <p class="font-normal text-xl leading-8 text-gray-500 max-lg:text-center max-w-2xl mx-auto">
+                    We are dedicated to delivering high-quality, innovative construction projects that are completed on time and within budget.
+We foster a collaborative and inclusive work environment that empowers our team and celebrates diversity.
+We prioritize safety and environmental responsibility in all our practices.
                     </p>
                 </div>
             </div>
             <div class="img-box ">
-                <img src="https://res.cloudinary.com/diulu4bz3/image/upload/v1718139122/mission_xvaigj.jpg" alt="About Us tailwind page"
-                    class=" lg:block " />
+                <img src={mission} alt="About Us tailwind page"
+                    class="hidden lg:block " />
             </div>
         </div>
     </div>
 </section>
-<section class="py-20 bg-white">
+<section class="py-20 bg-secondary">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <h2 class="font-manrope text-xl text-center text-gray-900 font-bold mb-14">Darna Capital result</h2>
+        <h2 class="font-manrope text-4xl text-center text-gray-900 font-bold mb-14">Darna Capital result</h2>
         <div class="flex flex-col gap-5 xl:gap-8 lg:flex-row lg:justify-between">
             <div
                 class="w-full max-lg:max-w-2xl mx-auto lg:mx-0 lg:w-1/3 bg-white p-6 rounded-2xl shadow-md shadow-gray-100">
@@ -124,7 +74,7 @@ together.
                     </div>
                     <div class="flex-1">
                         <h4 class="text-xl text-gray-900 font-semibold mb-2">Company growth</h4>
-                        <p class="text-xs text-blue-500 leading-5">Company's remarkable growth journey as we
+                        <p class="text-xs text-secondary leading-5">Company's remarkable growth journey as we
                             continually innovate and drive towards new heights of success.</p>
                     </div>
                 </div>
@@ -137,7 +87,7 @@ together.
                     </div>
                     <div class="flex-1">
                         <h4 class="text-xl text-gray-900 font-semibold mb-2">Company growth</h4>
-                        <p class="text-xs text-blue-500 leading-5">Our very talented team members are the powerhouse
+                        <p class="text-xs text-secondary leading-5">Our very talented team members are the powerhouse
                             of pagedone and pillars of our success. </p>
                     </div>
                 </div>
@@ -150,7 +100,7 @@ together.
                     </div>
                     <div class="flex-1">
                         <h4 class="text-xl text-gray-900 font-semibold mb-2">Projects Completed</h4>
-                        <p class="text-xs text-blue-500 leading-5">We have accomplished more than 625 projects
+                        <p class="text-xs text-secondary leading-5">We have accomplished more than 625 projects
                             worldwide and we are still counting many more.</p>
                     </div>
                 </div>
@@ -158,6 +108,7 @@ together.
         </div>
     </div>
 </section>
+
 
 
     </div>
