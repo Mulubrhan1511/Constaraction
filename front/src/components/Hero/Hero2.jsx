92fef7269f609ae4df1cf6f9ee48943318b8b6a9
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2024-06-18 13:23:46 +0300

    edit the hero section

--- front/src/components/Hero/Hero2.jsx
+++ front/src/components/Hero/Hero2.jsx
@@ -9,7 +9,7 @@ const Hero2 = () => {
           <div className='hero__text xl:w-[48%] text-center'>
             <div className="flex flex-col items-center">
             <h1 className='text-3xl font-bold mb-6 text-center xl:text-left'>DARNA Construction and Business PLC</h1>
-              <p className='mb-[42px] md:max-w-xl p-4 text-gold/85 bg-white bg-transparent/55 items-center justify-center rounded-md text-center'>
+              <p className='mb-[42px] md:max-w-xl p-4 text-gold/85 bg-white bg-transparent/50 items-center justify-center rounded-md text-center'>
                 DARNA specializes in a wide range of construction projects, including residential, 
                 commercial, and industrial developments. The company is known for its commitment to quality,
                 safety, and innovation, consistently delivering projects that exceed client expectations.
